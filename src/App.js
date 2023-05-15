c6cd4ea09531ee0998be68ecf244d8d205371daf
nabila <nabiladigo91@gmail.com>
2023-05-15 18:57:38 -0400

    creating react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import  Home from "./pages/Home";
+import  About from "./pages/About";
+import  Projects from "./pages/Projects";
+
+
+const App =() =>{
+  const URL = "https://digou-portfolio.herokuapp.com/";
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+             <Route exact path='/' element ={<Home  URL={URL} />} />
+            <Route path='/projects' element = {<Projects  URL={URL} />} />
+            <Route path='/about' element = {<About  URL={URL} /> } />
+            {/* <Route path='/contact' element= {<Contact URL={URL} />} />    */}
+            {/* <Route path='/resume' element= {<Resume URL={URL} />} />    */}
+        </Routes>
+        <Footer/>
     </div>
   );
 }
