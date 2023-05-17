e90122c8a3742d33dc4d4647c602ef6d7f0694a6
nabila <nabiladigo91@gmail.com>
2023-05-17 17:41:27 -0400

    frontend complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,27 @@
-function About(props) {
-    return <h1>About</h1>;
-  }
-  
-  export default About;
+import {useState, useEffect} from "react";
+
+function About(props){
+  const[about, setAbout] = useState(null);
+  const getAboutData = async() =>{
+    const response = await fetch(props.URL + "about");
+    const data = await response.json();
+    setAbout(data);
+  };
+  useEffect(()=>getAboutData(),[]);
+  const loaded= ()=>(
+    <div>
+      <img src={about.headshot} alt= {about.name} />
+      <div>
+        {about.bio}
+        <div>
+          <p>
+          Here are few technologies I've been working with: {about.skills}...
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+  return about ? loaded(): <h1>Loading...</h1>;
+};
+
+export default About;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,28 @@
-function Projects(props) {
-    return <h1>Projects</h1>;
-  }
-  
-  export default Projects;
+import { useState, useEffect } from "react";
+function Projects(props){
+  const [projects, setProjects]= useState(null);
+  const getProjectsData = async() =>{
+    const response= await fetch(props.URL + "projects");
+    const data= await response.json();
+    setProjects(data);
+  };
+  useEffect (()=> getProjectsData(), []);
+  const loaded = () =>{
+    return projects.map((project) => (
+      <div>
+        <h1>{project.title}</h1>
+        <div className="project_item-cta">
+          <img src={project.image}/>
+          <a href={project.git} className="btn" target="_blank">
+            Github
+          </a>
+          <a href={project.live} className="btn btn-primary" target="_blank">
+            Live site
+          </a>
+        </div>
+      </div>
+    ));
+  };
+  return projects ? loaded() : <h1> loading ...</h1>;
+}
+export default Projects;
